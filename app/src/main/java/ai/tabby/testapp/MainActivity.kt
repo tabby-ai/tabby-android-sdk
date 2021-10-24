d760d5fa1cbd017f79082c273debbd677b427579
GeekyVad <geekyvad@gmail.com>
2021-10-25 01:57:19 +0300

    project was created with default settings

---
+++ app/src/main/java/ai/tabby/testapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package ai.tabby.testapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ai.tabby.testapp.ui.theme.TabbySDKTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TabbySDKTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TabbySDKTheme {
+        Greeting("Android")
+    }
+}
